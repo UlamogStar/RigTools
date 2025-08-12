052e9613807394e0b9c8942c8b5ca1fc879c2876
Elijah <vigorilious@gmail.com>
2025-08-12 13:57:04 -0600

    Updated Color to Curve, RK set-up, and Constraint Tools

--- RigRefineTools/ConstrainTool.py
+++ RigRefineTools/ConstrainTool.py
@@ -1,35 +1,88 @@
 import maya.cmds as cmds
 
-def createParentConstraint():
-    selection = cmds.ls(selection=True)
-    if len(selection) != 2:
-        cmds.error('Please select two objects')
-        return
-    cmds.parentConstraint(selection[0], selection[1], mo=True, weight=1)
-def createScaleConstraint():
-    selection = cmds.ls(selection=True)
-    if len(selection) != 2:
-        cmds.error('Please select two objects')
-        return
-    cmds.scaleConstraint(selection[0], selection[1], mo=True, weight=1)
-def createOrientConstraint():
-    selection = cmds.ls(selection=True)
-    if len(selection) != 2:
-        cmds.error('Please select two objects')
-        return
-    cmds.orientConstraint(selection[0], selection[1], mo=True, weight=1)
-    
+WIN = 'constrainToolUI'
 
-def createUI():
-    if cmds.window('constrainUI', exists=True):
-        cmds.deleteUI('constrainUI', window=True)
-    
-    cmds.window('constrainUI', title='Constrain Tool', widthHeight=(200, 100), sizeable=False)
-    cmds.columnLayout(adjustableColumn=True)
-    cmds.button(label='Parent Constraint', command='createParentConstraint()')
-    cmds.button(label='Scale Constraint', command='createScaleConstraint()')
-    cmds.button(label='Orient Constraint', command='createOrientConstraint()')
+def _get_parent_and_children():
+    sels = cmds.ls(selection=True) or []
+    if len(sels) < 2:
+        cmds.warning('Select parent first, then one or more children.')
+        return None, []
+    return sels[0], sels[1:]
 
-    cmds.showWindow('constrainUI')
+def createParentConstraint(*args):
+    parent, children = _get_parent_and_children()
+    if not parent or not children:
+        return
+    mo = cmds.checkBox('ct_mo', q=True, v=True)
+    for child in children:
+        try:
+            cmds.parentConstraint(parent, child, mo=mo)
+        except Exception as e:
+            cmds.warning('ParentConstraint failed: {} -> {} | {}'.format(parent, child, e))
+
+def createOrientConstraint(*args):
+    parent, children = _get_parent_and_children()
+    if not parent or not children:
+        return
+    mo = cmds.checkBox('ct_mo', q=True, v=True)
+    for child in children:
+        try:
+            cmds.orientConstraint(parent, child, mo=mo)
+        except Exception as e:
+            cmds.warning('OrientConstraint failed: {} -> {} | {}'.format(parent, child, e))
+
+def createScaleConstraint(*args):
+    parent, children = _get_parent_and_children()
+    if not parent or not children:
+        return
+    for child in children:
+        try:
+            cmds.scaleConstraint(parent, child)
+        except Exception as e:
+            cmds.warning('ScaleConstraint failed: {} -> {} | {}'.format(parent, child, e))
+
+def clearConstraintsOnChildren(*args):
+    _, children = _get_parent_and_children()
+    if not children:
+        return
+    for child in children:
+        cons = []
+        # Constraints living under the child transform (typical)
+        cons += cmds.listRelatives(child, type='parentConstraint') or []
+        cons += cmds.listRelatives(child, type='orientConstraint') or []
+        cons += cmds.listRelatives(child, type='scaleConstraint') or []
+        # Also check connected constraints just in case
+        cons += cmds.listConnections(child, type='parentConstraint') or []
+        cons += cmds.listConnections(child, type='orientConstraint') or []
+        cons += cmds.listConnections(child, type='scaleConstraint') or []
+        cons = list(set(cons))
+        if cons:
+            cmds.delete(cons)
+
+def createUI(*args):
+    if cmds.window(WIN, exists=True):
+        cmds.deleteUI(WIN, window=True)
+
+    cmds.window(WIN, title='Constrain Tool', sizeable=False)
+    cmds.columnLayout(adj=True, rs=6)
+
+    cmds.text(label='Select parent, then child(ren).')
+    cmds.checkBox('ct_mo', label='Maintain Offset (Parent/Orient)', v=True)
+
+    cmds.rowLayout(numberOfColumns=3, adj=1,
+                   columnAttach=[(1,'both',0),(2,'both',0),(3,'both',0)],
+                   columnWidth=[(1,140),(2,140),(3,140)])
+    cmds.button(label='Parent Constraint', c='createParentConstraint()')
+    cmds.button(label='Orient Constraint', c='createOrientConstraint()')
+    cmds.button(label='Scale Constraint', c='createScaleConstraint()')
+    cmds.setParent('..')
+
+    cmds.separator(h=6, style='none')
+    cmds.button(label='Clear Constraints on Selected Children', c='clearConstraintsOnChildren()')
+
+    cmds.separator(h=6, style='none')
+    cmds.button(label='Refresh Selection', c='cmds.select(cmds.ls(sl=True))')
+
+    cmds.showWindow(WIN)
 
 createUI()
--- Rigging Tools/ControlCreationTool/ColorToCurve.py
+++ Rigging Tools/ControlCreationTool/ColorToCurve.py
@@ -1,46 +1,76 @@
 import maya.cmds as cmds
-def enableColorOveride():
+
+def enableColorOverride():
     sel = cmds.ls(sl=True)
     for obj in sel:
         cmds.setAttr(obj + '.overrideEnabled', 1)
 
 def selectCurvesObj():
     cmds.select(clear=True)
-    cmds.select(cmds.ls(type='nurbsCurve'))
+    curves = cmds.ls(type='nurbsCurve')
+    if curves:
+        parents = list(set(cmds.listRelatives(curves, parent=True, fullPath=True) or []))
+        cmds.select(parents)
+    else:
+        cmds.warning("No nurbsCurve shapes found.")
 
 def selectObj():
     cmds.select(clear=True)
     cmds.select(cmds.ls(sl=True))
-    
+
 def changeCurveColor(color):
-    if color < 0 or color > 31:
-        print("Value outside range, please enter value 0-31")
-    else:
-        sel = cmds.ls(sl=True)
-        for obj in sel:
-            shapes = cmds.listRelatives(obj, shapes=True)
-            if shapes:
-                for shape in shapes:
-                    cmds.setAttr(shape + '.overrideEnabled', 1)
-                    cmds.setAttr(shape + '.overrideColor', color)
-    changeObjColor
-def changeJointColor(color):
+    if color < 1 or color > 31:
+        print("Value outside range, please enter value 1-31")
+        return
     sel = cmds.ls(sl=True)
     for obj in sel:
-        cmds.setAttr(obj + '.overrideEnabled', 1)
-        cmds.setAttr(obj + '.overrideColor', color)
-                    
+        shapes = cmds.listRelatives(obj, shapes=True)
+        if shapes:
+            for shape in shapes:
+                if cmds.objectType(shape) == "nurbsCurve":
+                    cmds.setAttr(shape + '.overrideEnabled', 1)
+                    cmds.setAttr(shape + '.overrideColor', color)
+
+def changeJointColor(color):
+    if color < 1 or color > 31:
+        print("Value outside range, please enter value 1-31")
+        return
+    sel = cmds.ls(sl=True)
+    for obj in sel:
+        if cmds.objectType(obj) == "joint":
+            cmds.setAttr(obj + '.overrideEnabled', 1)
+            cmds.setAttr(obj + '.overrideColor', color)
+
+def updateCurveSwatch(*args):
+    colorIdx = cmds.intSlider('colorSlider', query=True, value=True)
+    cmds.symbolButton('curveColorSwatch', edit=True, image='colorIndex{}'.format(colorIdx))
+
+def updateJointSwatch(*args):
+    colorIdx = cmds.intSlider('jointColorSlider', query=True, value=True)
+    cmds.symbolButton('jointColorSwatch', edit=True, image='colorIndex{}'.format(colorIdx))
 
 def createUI():
     if cmds.window('colorToCurveUI', exists=True):
         cmds.deleteUI('colorToCurveUI', window=True)
 
-    cmds.window('colorToCurveUI', title='Color to Curve', widthHeight=(200, 100), sizeable=False)
+    cmds.window('colorToCurveUI', title='Color to Curve', widthHeight=(220, 180), sizeable=False)
     cmds.columnLayout(adjustableColumn=True)
-    cmds.button(label='Enable Color Override', command='enableColorOveride()')
-    cmds.intSlider('colorSlider', minValue=0, maxValue=31, value=0, step=1, cc='changeCurveColor(cmds.intSlider("colorSlider", query=True, value=True))')
-    cmds.button(label='Change Color', command='changeObjColor(cmds.intSlider("colorSlider", query=True, value=True))')
-    cmds.intSlider('jointColorSlider', minValue=0, maxValue=31, value=0, step=1, cc='changeJointColor(cmds.intSlider("jointColorSlider", query=True, value=True))')
+    cmds.button(label='Enable Color Override', command='enableColorOverride()')
+
+    cmds.rowLayout(numberOfColumns=2)
+    cmds.intSlider('colorSlider', minValue=1, maxValue=31, value=1, step=1,
+                   cc='changeCurveColor(cmds.intSlider("colorSlider", query=True, value=True));updateCurveSwatch()')
+    cmds.symbolButton('curveColorSwatch', width=40, height=20, image='colorIndex1', enable=False)
+    cmds.setParent('..')
+
+    cmds.button(label='Change Curve Color', command='changeCurveColor(cmds.intSlider("colorSlider", query=True, value=True))')
+
+    cmds.rowLayout(numberOfColumns=2)
+    cmds.intSlider('jointColorSlider', minValue=1, maxValue=31, value=1, step=1,
+                   cc='changeJointColor(cmds.intSlider("jointColorSlider", query=True, value=True));updateJointSwatch()')
+    cmds.symbolButton('jointColorSwatch', width=40, height=20, image='colorIndex1', enable=False)
+    cmds.setParent('..')
+
     cmds.button(label='Change Joint Color', command='changeJointColor(cmds.intSlider("jointColorSlider", query=True, value=True))')
     cmds.button(label='Select Object', command='selectObj()')
     cmds.button(label='Select Curves', command='selectCurvesObj()')
